252dfa679599cf5a64c1c8c64d5f3b43117123d9
Jan Civitelli Benso <jan.civitelli@dolphin-it.de>
2025-03-25 09:55:27 +0100

    add whitenoise

--- django_charge_summarizer/settings.py
+++ django_charge_summarizer/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     'django_celery_beat',
+    "whitenoise.runserver_nostatic",
+    "corsheaders",
 ]
 
 # Dynamic loading of modules
